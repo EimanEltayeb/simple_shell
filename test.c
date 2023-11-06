bd773ca4255835d576d62c4649f10a07e15d7b9f
GadourGuy <ogadours1@gmail.com>
2023-11-06 11:04:16 -0800

    uploading main.c & main.h

--- test.c
+++ test.c
@@ -32,29 +32,27 @@ int excute(char **arr, char *error)
 	char *path;
 
 	if (arr[0] == NULL || arr[0][0] == '\0')
-	{
 		return (0);
-	}
 	path = path_function(arr[0]);
 	if (path == NULL)
 	{
 		error_msg(arr, error);
-		return(127);
+		return (127);
 	}
-	else if(access(path, X_OK) != 0)
+	else if (access(path, X_OK) != 0)
 	{
 		error_permission(arr, error);
 		return (126);
 	}
 	else
-	{	
+	{
 		child = fork();
 		if (child == 0)
 		{
 			if (execve(path, arr, NULL) == -1 && arr[0][0] != ' ')
 			{
 				error_msg(arr, error);
-				exit(127);
+				return (127);
 			}
 				write(1, "\n", 1);
 			}
@@ -67,7 +65,22 @@ int excute(char **arr, char *error)
 			free(arr[j]);
 		free(arr);
 		}
-	return(0);
+	return (0);
+}
+/**
+ * free_memory - frees memory
+ * @arr: array to be freed
+ *
+ * Return: void
+ */
+
+void free_memory(char **arr)
+{
+	int j;
+
+	for (j = 0; arr[j] != NULL; j++)
+		free(arr[j]);
+	free(arr);
 }
 /**
  * main - main code
@@ -77,36 +90,27 @@ int excute(char **arr, char *error)
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	char *line = NULL, **arr, *token, *ex = "exit";
 	size_t n = 0;
-	int i = 0, j/*, result = 0*/;
+	int i = 0, result = 0;
 	ssize_t l = 0;
 
-	(void)argc;
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(1, "$ ", 2);
 		l = getline(&line, &n, stdin);
 		if (l == -1)
-		{
 			break;
-		}
 		if (l == 1 || (l == 2 && line[0] == '\n'))
-		{
 			continue;
-		}
 		arr = (char **)malloc(n * sizeof(char *));
 		token = strtok(line, " \n\t");
 		while (token != NULL)
 		{
-			if (token[0] != '\0')
-			{
-				arr[i] = strdup(token);
-				i++;
-			}
+			arr[i++] = strdup(token);
 			token = strtok(NULL, " \n\t");
 		}
 		arr[i] = NULL;
@@ -117,15 +121,15 @@ int main(int argc, char *argv[])
 		}
 		if (strcmp(arr[0], ex) == 0)
 		{
-			for (j = 0; arr[j] != NULL; j++)
-					free(arr[j]);
-			free(arr);
+			free_memory(arr);
 			break;
 		}
-		excute(arr, argv[0]);
+		result = excute(arr, argv[0]);
 		i = 0;
 	}
 	if (line != NULL)
 		free(line);
+	if (!(isatty(STDIN_FILENO)) && result != 0)
+		exit(127);
 	return (0);
 }
