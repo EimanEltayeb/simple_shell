8d24ae8e9d8de437d145efd8e01834b91ad3d161
EimanEltayeb <em_moh092@hotmail.com>
2023-11-06 09:27:24 +0300

    new main.h

--- main.h
+++ main.h
@@ -7,6 +7,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stats.h>
+#include <sys/stat.h>
+
+char *path_function(char *cmnd);
+int error_msg(char **arr, char *error);
+int error_permission(char **arr, char *error);
 
 #endif
