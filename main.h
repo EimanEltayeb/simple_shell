99b0fa341cd3dd2fea59bce80f080a6a1deadd29
EimanEltayeb <em_moh092@hotmail.com>
2023-11-07 06:30:01 +0300

    main

--- main.h
+++ main.h
@@ -8,10 +8,29 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+/**
+ * struct env_list - struct of environment
+ * @var: the variable of env
+ * @value: the value of the variable
+ * @next: the pointer to the next list
+*/
+extern char **environ;
+typedef struct env_list
+{
+	char *var;
+	char *value;
+	struct env_list *next;
+} env_list;
 
+env_list *env_struct(char **env);
 char *path_function(char *cmnd);
 int error_msg(char **arr, char *error);
 int error_permission(char **arr, char *error);
 int is_empty(const char *str);
+char *_getenv(const char *variable, env_list *head);
+int built(char *cmnd);
+
+
+
 
 #endif
